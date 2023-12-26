6ddc7567b53e86d9cb5417777cb5fc40d4096fa1
Jacob Brown <jacob.brown@unity3d.com>
2023-12-26 19:20:21 +0000

    Implement look ahead to throw out routes that create inaccessible squares

--- lattice-numbering/RouteFinders/IndexRouteFinder.cs
+++ lattice-numbering/RouteFinders/IndexRouteFinder.cs
@@ -5,18 +5,21 @@ namespace LatticeNumbering.RouteFinders;
 public class IndexRouteFinder : IRouteFinder
 {
     private readonly int _n;
+    private readonly int _d;
     
     private bool[] _squares = null!;
-    
+    private bool[] _middleSquares = null!;
+
     private int _remainingCount;
     private int _middleCount;
 
     private int _middleRemainingOptimisationCount;
     private int _breakingMoveOptimisationCount;
     
-    public IndexRouteFinder(int n)
+    public IndexRouteFinder(int n, int d)
     {
         _n = n;
+        _d = d;
     }
     
     public int Run()
@@ -26,10 +29,16 @@ public class IndexRouteFinder : IRouteFinder
         _middleCount = (_n - 2) * (_n - 2);
         
         _squares = new bool[_remainingCount];
-        Array.Fill(_squares, false);
+        _middleSquares = new bool[_remainingCount];
+        
+        for (var i = 0; i < _remainingCount; i++)
+        {
+            _squares[i] = false;
+            _middleSquares[i] = i.IsMiddleSquare(_n);
+        }
         
         // Only going one direction from the origin corner (downwards, right is disconnected)
-        var cornerRouteCount = VisitNode(0);
+        var cornerRouteCount = MoveToSquare(0);
         Console.WriteLine("Optimisation method aborted the following number of routes");
         Console.WriteLine($"Middle square remaining optimisation: {_middleRemainingOptimisationCount}");
         Console.WriteLine($"Breaking move optimisation: {_breakingMoveOptimisationCount}");
@@ -37,92 +46,108 @@ public class IndexRouteFinder : IRouteFinder
         return cornerRouteCount * 8;
     }
     
-    private int VisitNode(int thisIndex)
+    private int MoveToSquare(int thisIndex)
     {
-        _squares[thisIndex] = true;
-        _remainingCount--;
-
-        if (thisIndex.IsMiddleSquare(_n))
-            _middleCount--;
-
         var count = 0;
-            
+        
+        // Update grid metrics
+        SetSquareLock(thisIndex, true);
+
         // Determine whether there are any more squares remaining to visit
         if (_remainingCount > 0)
         {
-            var nextSquares = GetNextSquares(thisIndex);
-            if (IsRoutePossible(nextSquares))
+            // Investigate each node connected to this one that has not already been visited by this route
+            foreach (var nextNode in GetNextSquares(thisIndex))
             {
-                // Investigate each node connected to this one that has not already been visited by this route
-                foreach (var nextNode in nextSquares)
-                {
-                    count += VisitNode(nextNode);
-                }
+                if(VerifyRouteAhead(nextNode, _d))
+                    count += MoveToSquare(nextNode);
+            }
+        }
+        else if (_remainingCount > 0)
+        {
+            // Investigate each node connected to this one that has not already been visited by this route
+            foreach (var nextNode in GetNextSquares(thisIndex))
+            {
+                count += MoveToSquare(nextNode);
             }
         }
         else
         {
             // If this node ends on a middle square then it is a valid route
-            if(thisIndex.IsMiddleSquare(_n))
+            if(_middleSquares[thisIndex])
                 count++;
         }
 
         // Allow this node to be visited by other routes
-        _squares[thisIndex] = false;
-        _remainingCount++;
-        _middleCount++;
+        SetSquareLock(thisIndex, false);
         
         return count;
     }
 
-    private bool IsRoutePossible(List<int> nextSquares)
+    private void SetSquareLock(int index, bool isLocked)
     {
-        // Determine whether there are any more middle squares remaining to visit
-        // If there are none remaining then the route is already impossible
-        if (_middleCount == 0)
+        if (isLocked)
         {
-            _middleRemainingOptimisationCount++;
-            return false;
-        }
-        
-        // If there is more than one option available, verify that other adjacent squares are not left inaccessible
-        if (nextSquares.Count > 1)
-        {
-            foreach (var verificationSquareIndex in nextSquares)
+            _squares[index] = true;
+            _remainingCount--;
+            if (_middleSquares[index])
             {
-                if (!VerifySquare(verificationSquareIndex))
-                {
-                    _breakingMoveOptimisationCount++;
-                    return false;
-                }
+                _middleCount--;
             }
         }
-
-        return true;
+        else
+        {
+            _squares[index] = false;
+            _remainingCount++;
+            if (_middleSquares[index])
+            {
+                _middleCount++;
+            }
+        }
     }
 
-    // Return true if the square is 'valid'
-    // Valid = is already visited, or has at least one available route
-    private bool VerifySquare(int index)
+    // Verify a square has at least one available route remaining
+    private bool VerifyRouteAhead(int index, int remainingDepth)
     {
-        // Square is considered valid if it has already been visited
-        if (_squares[index])
+        if (remainingDepth <= 0)
             return true;
-
-        _squares[index] = true;
-
-        var nextInRoute = GetNextSquares(index);
-
-        var isPossible = nextInRoute.Count > 0;
-
-        if (!isPossible)
-            _breakingMoveOptimisationCount++;
         
-        // TODO Recursively run VerifySquare here to verify that the next square is accessible to (to d depth)
+        SetSquareLock(index, true);
 
-        _squares[index] = false;
+        // Determine whether there are any more squares remaining to visit
+        if (_remainingCount > 0)
+        {
+            // If there are no middle squares in the remaining squares then route is invalid
+            if (_middleCount == 0)
+            {
+                _middleRemainingOptimisationCount++;
+                SetSquareLock(index, false);
+                return false;
+            }
 
-        return isPossible;
+            // If this move is a dead end then the route is invalid
+            var nextSquares = GetNextSquares(index);
+            if (nextSquares.Count == 0)
+            {
+                _breakingMoveOptimisationCount++;
+                SetSquareLock(index, false);
+                return false;
+            }
+
+            // Verify whether there are any possible future routes that make this route worth exploring
+            var isFutureRoutePossible = false;
+            foreach (var nextSquare in nextSquares)
+            {
+                if(!isFutureRoutePossible)
+                    isFutureRoutePossible = isFutureRoutePossible || VerifyRouteAhead(nextSquare, remainingDepth - 1);
+            }
+            SetSquareLock(index, false);
+            return isFutureRoutePossible;
+        }
+        
+        // Allow this node to be visited by other routes
+        SetSquareLock(index, false);
+        return true;
     }
     
     private List<int> GetNextSquares(int index)
