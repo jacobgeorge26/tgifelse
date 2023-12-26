8787518e38ed3e651e379d07646ceae44fa82e79
Jacob Brown <jacob.brown@unity3d.com>
2023-12-26 01:36:15 +0000

    Implement sample square optimisation

--- lattice-numbering/RouteFinders/IndexRouteFinder.cs
+++ lattice-numbering/RouteFinders/IndexRouteFinder.cs
@@ -12,6 +12,9 @@ public class IndexRouteFinder : IRouteFinder
     private int _remainingCount;
     private int _middleCount;
 
+    private int _skippedMiddleCount = 0;
+    private int _skippedSampleCount = 0;
+    
     public IndexRouteFinder(int n)
     {
         _n = n;
@@ -19,13 +22,23 @@ public class IndexRouteFinder : IRouteFinder
     
     public int Run()
     {
-        _squares = new bool[_squareCount];
         // Total number of squares, and of those middle squares, are available across the entire grid
         _remainingCount = _n * _n;
         _middleCount = (_n - 2) * (_n - 2);
+        
+        _squares = new bool[_remainingCount];
         Array.Fill(_squares, false);
+        
+        // Get indices of corner squares (excluding the origin corner)
+        _verificationSquareIndices = new []{_n - 1, _remainingCount - _n, _remainingCount - 1};
 
-        return VisitNode(0) * 8;
+        // Only going one direction from the origin corner (downwards, right is disconnected)
+        var cornerRouteCount = VisitNode(0);
+        Console.WriteLine("Optimisation method aborted the following number of routes");
+        Console.WriteLine($"Middle square optimisation: {_skippedMiddleCount}");
+        Console.WriteLine($"Sample square optimisation: {_skippedSampleCount}");
+        
+        return cornerRouteCount * 8;
     }
     
     private int VisitNode(int thisIndex)
@@ -41,9 +54,7 @@ public class IndexRouteFinder : IRouteFinder
         // Determine whether there are any more squares remaining to visit
         if (_remainingCount > 0)
         {
-            // Determine whether there are any more middle squares remaining to visit
-            // If there are none remaining then the route is already impossible
-            if (_middleCount > 0)
+            if (IsRoutePossible())
             {
                 // Investigate each node connected to this one that has not already been visited by this route
                 foreach (var nextNode in GetNextSquares(thisIndex))
@@ -67,7 +78,51 @@ public class IndexRouteFinder : IRouteFinder
         return count;
     }
 
-    private IEnumerable<int> GetNextSquares(int index)
+    private bool IsRoutePossible()
+    {
+        // Determine whether there are any more middle squares remaining to visit
+        // If there are none remaining then the route is already impossible
+        if (_middleCount == 0)
+        {
+            _skippedMiddleCount++;
+            return false;
+        }
+
+        // Verify that all the squares in the sample pool are still accessible
+        foreach (var verificationSquareIndex in _verificationSquareIndices)
+        {
+            if (!VerifySquare(verificationSquareIndex))
+                return false;
+        }
+
+        return true;
+    }
+
+    // Return true if the square is 'valid'
+    // Valid = is already visited, or has at least one available route
+    private bool VerifySquare(int index)
+    {
+        // Square is considered valid if it has already been visited
+        if (_squares[index])
+            return true;
+
+        _squares[index] = true;
+
+        var nextInRoute = GetNextSquares(index);
+
+        var isPossible = nextInRoute.Count > 0;
+
+        if (!isPossible)
+            _skippedSampleCount++;
+        
+        // TODO Recursively run VerifySquare here to verify that the next square is accessible to (to d depth)
+
+        _squares[index] = false;
+
+        return isPossible;
+    }
+    
+    private List<int> GetNextSquares(int index)
     {
         var availableSquares = new List<int>();
         
